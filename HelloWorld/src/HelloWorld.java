c6e14fe89cbbe606e6e54229cc148ddb6a189feb
Donny <masterlink94@gmail.com>
2016-05-07 16:42:41 -0700

    added some sick functionality breh.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,11 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("HELLOOOOOO!!!!?????");
+		
+		for(int i = 0; i < 100; i++){
+			System.out.println("ELLLOOoooooo.....");
+		}
 	}
+	
+	
 }
